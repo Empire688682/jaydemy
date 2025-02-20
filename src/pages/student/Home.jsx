539b50ba6f4be7524aedc36d2d408d68de3f983c
Empire688682 <asehindej@gmail.com>
2025-02-20 03:57:04 +0100

    assets

--- src/pages/student/Home.jsx
+++ src/pages/student/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Navbar from '../../Component/student/navbar'
 
 const Home = () => {
   return (
     <div>
+        <Navbar/>
       <h1>Home section</h1>
     </div>
   )
