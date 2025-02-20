8e0bbd7b69c0ac298cad70cc0c27c068d127aef8
Empire688682 <asehindej@gmail.com>
2025-02-20 03:25:32 +0100

    routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,37 @@
 import React from 'react'
 import { UseGlobalContext } from './Context'
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/student/Home';
+import CoursesList from './pages/student/CoursesList';
+import CourseDetails from './pages/student/CourseDetails';
+import MyEnrollment from './pages/student/MyEnrollment';
+import Player from './pages/student/Player';
+import Loading from './Component/student/Loading';
+import Educator from './pages/educator/Educator';
+import Dashboard from './pages/educator/Dashboard';
+import AddCourse from './pages/educator/AddCourse';
+import StudentEnrolled from './pages/educator/StudentEnrolled';
+import MyCourses from './pages/educator/MyCourses';
 
 const App = () => {
   const {hello} = UseGlobalContext();
   return (
     <div>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/course-list' element={<CoursesList/>}/>
+        <Route path='/course-list/:id' element={<CoursesList/>}/>
+        <Route path='/course/:id' element={<CourseDetails/>}/>
+        <Route path='/my-enrollment' element={<MyEnrollment/>}/>
+        <Route path='/player/:coursId' element={<Player/>}/>
+        <Route path='/loading/:pth' element={<Loading/>}/>
+        <Route path='/educator' element={<Educator/>}>
+         <Route path='/educator' element={<Dashboard/>}/>
+         <Route path='/add-course' element={<AddCourse/>}/>
+         <Route path='/student-enrolled' element={<StudentEnrolled/>}/>
+         <Route path='/my-courses' element={<MyCourses/>}/>
+        </Route>
+      </Routes>
     </div>
   )
 }
