6109a5e131ee888746b35b6fa1b1264ddcf047ca
Empire688682 <asehindej@gmail.com>
2025-02-24 04:07:04 +0100

    company

--- src/Component/student/TrustedCompany.jsx
+++ src/Component/student/TrustedCompany.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { assets } from '../../assets/assets'
 
 const TrustedCompany = () => {
   return (
-    <div>
-      <h1>Trusted Company Section</h1>
+    <div className='pt-16'>
+      <p className='text-base text-gray-500'>Trusted by learners from</p>
+      <div className='flex items-center justify-center flex-wrap gap-6 md:gap-16 md:mt-10 mt-5 '>
+        <img src={assets.microsoft_logo} className='w-20 md:w-28' alt="Microsoft" />
+        <img src={assets.walmart_logo} className='w-20 md:w-28' alt="Walmart" />
+        <img src={assets.accenture_logo} className='w-20 md:w-28' alt="Accenture" />
+        <img src={assets.adobe_logo} className='w-20 md:w-28' alt="adobe" />
+        <img src={assets.paypal_logo} className='w-20 md:w-28' alt="Paypal" />
+      </div>
     </div>
   )
 }
--- src/pages/student/Home.jsx
+++ src/pages/student/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Hero from '../../Component/student/hero'
+import TrustedCompany from '../../Component/student/TrustedCompany'
+import CoursesList from './CoursesList'
 
 const Home = () => {
   return (
-    <div className=''>
+    <div className='flex flex-col items-center space-y-7 text-center'>
       <Hero/>
+      <TrustedCompany/>
+      <CoursesList/>
     </div>
   )
 }
