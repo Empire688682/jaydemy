af39cdc665d765432f0215752744a29abfa1dc5d
Empire688682 <asehindej@gmail.com>
2025-02-24 03:46:35 +0100

    heroFunction

--- src/Component/student/Hero.jsx
+++ src/Component/student/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { assets } from '../../assets/assets'
+import SearchBar from './SearchBar'
 
 const Hero = () => {
   return (
@@ -7,6 +8,7 @@ const Hero = () => {
       <h1 className='md:text-home-heading-large text-home-heading-small relative font-bold text-gray-800 max-w-3xl mx-auto'>Empower your future with the curses designed to <span className='text-blue-600'>fit your choice</span> <img src={assets.sketch} className='md:block hidden absolute -bottom-7 right-0' alt='sketch'/> </h1>
       <p className='md:block hidden text-gray-500 max-w-2xl mx-auto'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem aut fugiat eveniet nesciunt sunt nostrum? Vitae sunt ex dolores error, culpa dignissimos, non quas veritatis architecto, ullam cumque eius soluta?</p>
       <p className='md:hidden text-gray-500 max-w-sm mx-auto'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem aut fugiat eveniet nesciunt sunt nostrum?</p>
+      <SearchBar/>
     </div>
   )
 }
--- src/Component/student/SearchBar.jsx
+++ src/Component/student/SearchBar.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { assets } from '../../assets/assets'
+import { useNavigate } from 'react-router-dom'
 
-const SearchBar = () => {
+const SearchBar = ({data}) => {
+  const navigate = useNavigate();
+  const  [input, setIbput] = useState(data? data:"");
+
+  const onSearchHandler = (e) =>{
+    e.preventDefault();
+    navigate("/courses-list/"+ input);
+  }
   return (
-    <div>
-      <h1>Seach Bar Section</h1>
-    </div>
+      <form onSubmit={onSearchHandler} className='flex max-w-xl w-full md:h-14 h-12 items-center bg-white border rounded border-gray-500/20'>
+        <img src={assets.search_icon} alt="search_icon" className='md:w-auto w-10 px-3' />
+        <input type="text" onChange={(e)=>setIbput(e.target.value)} value={input} placeholder='Search for curses' className='w-full h-full outline-none text-gray-500/80' />
+        <button type='submit' className='bg-blue-600 rounded text-white md:px-10 px-7 md:py-3 py-2 mx-1'>Search</button>
+      </form>
   )
 }
 
