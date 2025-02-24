5dea82d607018b155c7a06a38de9ef6cec9276f5
Empire688682 <asehindej@gmail.com>
2025-02-24 04:28:03 +0100

    coursList

--- src/Component/student/CoursesSection.jsx
+++ src/Component/student/CoursesSection.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const CoursesSection = () => {
   return (
-    <div>
-      <h1>Courses Sectiom</h1>
+    <div className='py-16 md:py-40 px-8'>
+      <h2>Learn from the best</h2>
+      <p className='text-sm md:text-base text-gray-500 mt-3'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente, tenetur, maiores mollitia amet totam, dolorem cumque numquam natus reiciendis necessitatibus quia saepe commodi?</p>
+      <Link to="/course-list" onClick={()=> scrollTo(0,0)} className='text-gray-500 border m-y-10 border-gray-500/30 px-10 py-3 rounded '>See all courses</Link>
     </div>
   )
 }
--- src/pages/student/Home.jsx
+++ src/pages/student/Home.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import Hero from '../../Component/student/hero'
 import TrustedCompany from '../../Component/student/TrustedCompany'
 import CoursesList from './CoursesList'
+import CourseDetails from './CourseDetails'
+import CoursesSection from '../../Component/student/CoursesSection'
 
 const Home = () => {
   return (
     <div className='flex flex-col items-center space-y-7 text-center'>
       <Hero/>
       <TrustedCompany/>
+      <CoursesSection/>
       <CoursesList/>
     </div>
   )
