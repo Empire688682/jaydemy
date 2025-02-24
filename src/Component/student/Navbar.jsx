b4e25dd896145aa8057c41fe3243134823f79586
Empire688682 <asehindej@gmail.com>
2025-02-24 02:05:33 +0100

    clerk

--- src/Component/student/Navbar.jsx
+++ src/Component/student/Navbar.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { assets } from '../../assets/assets';
 import { Link } from 'react-router-dom';
-
+import { useClerk, UserButton, useUser } from '@clerk/clerk-react';
 
 const Navbar = () => {
-    const isCourslistPage = location.pathname.includes("/course-list")
+    const isCourslistPage = location.pathname.includes("/course-list");
+
+    const {openSignIn} = useClerk();
+    const {user} = useUser();
+
   return (
     <div className={`flex items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 ${isCourslistPage ? "bg-white" : "bg-cyan-100/70"} py-3`}>
         <img src={assets.logo} alt="logo" className='w-28 lg:w-32 cursor-pointer'/>
@@ -13,13 +17,16 @@ const Navbar = () => {
                 <button>Become Educator</button>
                 |<Link to="/my-enrollment">My Enrollment</Link>
             </div>
-            <button className='bg-blue-600 text-white px-5 py-2 rounded-full'>Create account</button>
+            {
+              user ? <UserButton/> :
+              <button className='bg-blue-600 text-white px-5 py-2 rounded-full' onClick={()=>openSignIn()}>Create account</button>
+            }
         </div>
         {/*For Mobile View */}
         <div className='md:hidden flex items-center gap-2 sm:gap-5 text-gray-500'>
         <button>Become Educator</button>
         |<Link to="/my-enrollment">My Enrollment</Link>
-        <button><img src={assets.user_icon} alt="img" /></button>
+        <button onClick={()=>openSignIn()}><img src={assets.user_icon} alt="img" /></button>
         </div>
     </div>
   )
